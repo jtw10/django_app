bee68078e6559280df2d7a4a8f879d158a4bb2a0
Josh W <jtwong10@gmail.com>
2019-06-04 16:18:37 -0700

    update

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -1,15 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 from .models import Board
+
 
 def home(request):
     boards = Board.objects.all()
-    boards_names = list()
-
-    for board in boards:
-        boards_names.append(board.name)
-
-    response_html = '<br>'.join(boards_names)
-
-
-    return HttpResponse('Pepsi is useless, why should I drink Pepsi!')
+    return render(request, 'home.html', {'boards': boards})
