c169b85437b392fe9e64c5e3d63e011d69eb8a8c
Josh W <jtwong10@gmail.com>
2019-06-06 12:35:09 -0700

    update

--- myproject/boards/models.py
+++ myproject/boards/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
-
 
 class Board(models.Model):
     name = models.CharField(max_length=30, unique=True)
