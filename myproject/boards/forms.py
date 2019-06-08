2191b6c7fb517cf8acf254b1f4d3666d6db5367a
Josh W <jtwong10@gmail.com>
2019-06-08 16:25:13 -0700

    update

---
+++ myproject/boards/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from .models import Topic
+
+
+class NewTopicForm(forms.ModelForm):
+    message = forms.CharField(
+        widget=forms.Textarea(
+            attrs={'rows': 5, 'placeholder': 'Enter your message here.'}
+        ),
+        max_length=4000,
+        help_text='The maximum character count is 4000.'
+    )
+
+    class Meta:
+        model = Topic
+        fields = ['subject', 'message']
