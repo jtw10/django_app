2f5cc61cbc87688907ea066159c4d7982b658b5c
Josh W <jtwong10@gmail.com>
2019-06-08 14:29:41 -0700

    unit testing for new posts

--- myproject/boards/tests.py
+++ myproject/boards/tests.py
@@ -1,8 +1,9 @@
 from django.urls import reverse
 from django.test import TestCase
 from django.urls import resolve
+from django.contrib.auth.models import User
 from .views import home, board_topics, new_topic
-from .models import Board
+from .models import Board, Topic, Post
 
 
 class HomeTests(TestCase):
@@ -40,6 +41,16 @@ class BoardTopicsTest(TestCase):
         homepage_url = reverse('home')
         self.assertContains(response, 'href="{0}"'.format(homepage_url))
 
+    def test_board_topics_view_contains_navigation_links(self):
+        board_topics_url = reverse('board_topics', kwargs={'pk': 1})
+        homepage_url = reverse('home')
+        new_topic_url = reverse('new_topic', kwargs={'pk': 1})
+
+        response = self.client.get(board_topics_url)
+
+        self.assertContains(response, 'href="{0}"'.format(homepage_url))
+        self.assertContains(response, 'href="{0}"'.format(new_topic_url))  
+
 
 class HomeTests(TestCase):
     def setUp(self):
@@ -62,6 +73,7 @@ class HomeTests(TestCase):
 class NewTopicTest(TestCase):
     def setUp(self):
         Board.objects.create(name='Unit Test', description='Unit test board.')
+        User.objects.create_user(username='mary', email='maryjane@johnsmith.com', password='123')
 
     def test_new_topic_view_success_status_code(self):
         url = reverse('new_topic', kwargs={'pk': 1})
@@ -82,3 +94,42 @@ class NewTopicTest(TestCase):
         board_topics_url = reverse('board_topics', kwargs={'pk': 1})
         response = self.client.get(new_topic_url)
         self.assertContains(response, 'href="{0}"'.format(board_topics_url))
+
+    def test_csrf(self):
+        url = reverse('new_topic', kwargs={'pk': 1})
+        response = self.client.get(url)
+        self.assertContains(response, 'csrfmiddlewaretoken')
+
+    def test_new_topic_valid_post_data(self):
+        url = reverse('new_topic', kwargs={'pk': 1})
+        data = {
+            'subject': 'Test title',
+            'message': 'Lorem ipsum dolor sit amet'
+        }
+        response = self.client.post(url, data)
+        self.assertTrue(Topic.objects.exists())
+        self.assertTrue(Post.objects.exists())
+
+    def test_new_topic_invalid_post_data(self):
+        '''
+        Invalid post data should not redirect
+        The expected behavior is to show the form again with validation errors
+        '''
+        url = reverse('new_topic', kwargs={'pk': 1})
+        response = self.client.post(url, {})
+        self.assertEquals(response.status_code, 200)
+
+    def test_new_topic_invalid_post_data_empty_fields(self):
+        '''
+        Invalid post data should not redirect
+        The expected behavior is to show the form again with validation errors
+        '''
+        url = reverse('new_topic', kwargs={'pk': 1})
+        data = {
+            'subject': '',
+            'message': ''
+        }
+        response = self.client.post(url, data)
+        self.assertEquals(response.status_code, 200)
+        self.assertFalse(Topic.objects.exists())
+        self.assertFalse(Post.objects.exists())
--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.models import User
 from django.http import Http404
-from .models import Board
+from .models import Board, Topic, Post
 
 
 def home(request):
@@ -24,4 +25,25 @@ def board_topics(request, pk):
 
 def new_topic(request, pk):
     board = get_object_or_404(Board, pk=pk)
+
+    if request.method == 'POST':
+        subject = request.POST['subject']
+        message = request.POST['message']
+
+        user = User.objects.first()  # TODO: get logged in user
+
+        topic = Topic.objects.create(
+            subject=subject,
+            board=board,
+            starter=user
+        )
+
+        post = Post.objects.create(
+            message=message,
+            topic=topic,
+            created_by=user
+        )
+
+        return redirect('board_topics', pk=board.pk)  # TODO: redirect to the created topic page
+
     return render(request, 'new_topic.html', {'board': board})
