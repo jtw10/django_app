eabcfd8971b692aa549e30bbe40a15d66305e526
Josh W <jtwong10@gmail.com>
2019-06-09 16:58:03 -0700

    update

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 from boards import views
 from accounts import views as accounts_views
 
@@ -25,4 +26,6 @@ urlpatterns = [
     url(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
     url(r'^admin/', admin.site.urls),
     url(r'^signup/$', accounts_views.signup, name='signup'),
+    url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
+    url(r'^login/$', auth_views.LoginView.as_view(template_name='login.html')),
 ]
