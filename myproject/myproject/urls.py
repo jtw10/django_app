c7fe337f3683bb8d22fa49ebaca79c394298e3a4
Josh W <jtwong10@gmail.com>
2019-06-09 17:56:53 -0700

    update

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -27,5 +27,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^signup/$', accounts_views.signup, name='signup'),
     url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
-    url(r'^login/$', auth_views.LoginView.as_view(template_name='login.html')),
+    url(r'^login/$', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
 ]
