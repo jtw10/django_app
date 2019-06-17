fdc5e284631fc938132eb10215bc4cc5bd2b3243
Josh W <jtwong10@gmail.com>
2019-06-16 18:03:28 -0700

    update

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.http import Http404
+from django.utils import timezone
 from django.db.models import Count
+from django.views.generic import View, UpdateView
 from .models import Board, Topic, Post
 from .forms import NewTopicForm, PostForm
 
@@ -68,3 +70,34 @@ def reply_topic(request, pk, topic_pk):
     else:
         form = PostForm()
     return render(request, 'reply_topic.html', {'topic': topic, 'form': form})
+
+
+class NewPostView(View):
+    def render(self, request):
+        return render(request, 'new_post.html', {'form': self.form})
+
+    def post(self, request):
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('post_list')
+        return render(request, 'new_post.html', {'form': form})
+
+    def get(self, request):
+        form = PostForm()
+        return render(request, 'new_psot.html', {'form': form})
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = ('message', )
+    template_name = 'edit_post.html'
+    pk_url_kwarg = 'post_pk'
+    context_object_name = 'post'
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.updated_by = self.request.user
+        post.updated_at = timezone.now()
+        post.save()
+        return redirect('topic_posts', pk=post.topic.board.pk, topic_pk=post.topic.pk)
