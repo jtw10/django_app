d55a2f802d6727d6d6cdacaca1d854fd4c4dabbb
Josh W <jtwong10@gmail.com>
2019-06-20 19:43:30 -0700

    sessions

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -42,8 +42,13 @@ class PostListView(ListView):
     paginate_by = 5
 
     def get_context_data(self, **kwargs):
-        self.topic.views += 1
-        self.topic.save()
+
+        session_key = 'viewed_topic_{}'.format(self.topic.pk)
+        if not self.request.session.get(session_key, False):
+            self.topic.views += 1
+            self.topic.save()
+            self.request.session[session_key] = True
+
         kwargs['topic'] = self.topic
         return super().get_context_data(**kwargs)
 
