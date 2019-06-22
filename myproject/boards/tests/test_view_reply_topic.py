e0018430aaa6197754e65fdd0ca93bd3b5423c56
Josh W <jtwong10@gmail.com>
2019-06-22 15:24:50 -0700

    update

--- myproject/boards/tests/test_view_reply_topic.py
+++ myproject/boards/tests/test_view_reply_topic.py
@@ -57,7 +57,8 @@ class SuccessfulReplyTopicTests(ReplyTopicTestCase):
         self.response = self.client.post(self.url, {'message': 'Jim Bob'})
 
     def test_redirection(self):
-        topic_posts_url = reverse('topic_posts', kwargs={'pk': self.board.pk, 'topic_pk': self.topic.pk})
+        url = reverse('topic_posts', kwargs={'pk': self.board.pk, 'topic_pk': self.topic.pk})
+        topic_posts_url = '{url}?page=1#2'.format(url=url)
         self.assertRedirects(self.response, topic_posts_url)
 
     def test_reply_created(self):
