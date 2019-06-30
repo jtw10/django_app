8f64bbcfb0352790d01253f9eb484d2c0d0c90a1
Josh W <jtwong10@gmail.com>
2019-06-29 22:13:05 -0700

    update

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -126,6 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'staticfiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
@@ -140,4 +141,4 @@ LOGIN_URL = 'login'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # activate Heroku settings for django
-django_heroku.settings(local())
+django_heroku.settings(locals())
