bf32f00b8d864301eda5ab3372c668707b9abd7d
Josh W <jtwong10@gmail.com>
2019-06-29 22:21:51 -0700

    rearranging files

--- myproject/old/functions.py
+++
@@ -1,8 +0,0 @@
-from tkinter import *
-from tkinter import simpledialog
-from tkinter import messagebox
-
-def createprofile():
-    name = simpledialog.askstring("Question 1/10", "What is your name?")
-    age = simpledialog.askinteger("Question 2/10", "How old are you?")
-    print(name, age)
