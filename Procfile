04ab7e6ce5d24ff5cf337a10cff4fae59721b1a3
Josh W <jtwong10@gmail.com>
2019-06-29 22:49:54 -0700

    update

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn projectname.wsgi --log-file -
+web: gunicorn myproject.wsgi --log-file -
--- myproject/settings.py
+++ myproject/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.humanize',
     'accounts',
     'widget_tweaks',
-    'boards'
+    'boards',
 ]
 
 MIDDLEWARE = [
